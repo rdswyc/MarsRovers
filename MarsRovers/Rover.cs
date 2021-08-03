dd277d289b52aef61192a001e8497902dd7758c9
Rodrigo Chin <rodrigo.chin@outlook.com>
2021-08-03 11:47:54 -0300

    Add rover behaviour based on move instruction

--- MarsRovers/Rover.cs
+++ MarsRovers/Rover.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Text.RegularExpressions;
 
 namespace MarsRovers
@@ -12,10 +12,52 @@ namespace MarsRovers
             Z = z;
         }
 
+        public void Move(in Instruction instruction)
+        {
+            switch (instruction)
+            {
+                case Instruction.L:
+                    TurnLeft();
+                    break;
+                case Instruction.M:
+                    GoFoward();
+                    break;
+                case Instruction.R:
+                    TurnRight();
+                    break;
+                default:
+                    break;
+            }
+        }
+
         public override string ToString()
         {
             return $"{X} {Y} {Z}";
         }
+
+        private void GoFoward()
+        {
+            switch (Z)
+            {
+                case Heading.N:
+                    Y++;
+                    break;
+                case Heading.E:
+                    X++;
+                    break;
+                case Heading.S:
+                    Y--;
+                    break;
+                case Heading.W:
+                    X--;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void TurnLeft() => Z = Z == Heading.N ? Heading.W : Z - 1;
+        private void TurnRight() => Z = Z == Heading.W ? Heading.N : Z + 1;
 
         public static bool TryParse(string s, out Rover result)
         {
--- Tests/RoverTest.cs
+++ Tests/RoverTest.cs
@@ -6,6 +6,76 @@ namespace Tests
     public class RoverTest
     {
         [Theory]
+        [InlineData(Heading.N, Heading.W)]
+        [InlineData(Heading.E, Heading.N)]
+        [InlineData(Heading.S, Heading.E)]
+        [InlineData(Heading.W, Heading.S)]
+        public void Move_Left_ShouldUpdateHeading(Heading heading, Heading newHeading)
+        {
+            var rover = new Rover(1, 2, heading);
+
+            rover.Move(Instruction.L);
+
+            Assert.Equal(newHeading, rover.Z);
+        }
+
+        [Theory]
+        [InlineData(Heading.N, Heading.E)]
+        [InlineData(Heading.E, Heading.S)]
+        [InlineData(Heading.S, Heading.W)]
+        [InlineData(Heading.W, Heading.N)]
+        public void Move_Right_ShouldUpdateHeading(Heading heading, Heading newHeading)
+        {
+            var rover = new Rover(1, 2, heading);
+
+            rover.Move(Instruction.R);
+
+            Assert.Equal(newHeading, rover.Z);
+        }
+
+        [Theory]
+        [InlineData(Instruction.L)]
+        [InlineData(Instruction.R)]
+        public void Move_LeftOrRight_ShouldNotChangePosition(Instruction instruction)
+        {
+            var rover = new Rover(1, 2, Heading.N);
+
+            rover.Move(instruction);
+
+            Assert.Equal(1, rover.X);
+            Assert.Equal(2, rover.Y);
+        }
+
+        [Theory]
+        [InlineData(Heading.N, 1, 3)]
+        [InlineData(Heading.E, 2, 2)]
+        [InlineData(Heading.S, 1, 1)]
+        [InlineData(Heading.W, 0, 2)]
+        public void Move_Forward_ShouldUpdatePosition(Heading heading, byte newX, byte newY)
+        {
+            var rover = new Rover(1, 2, heading);
+
+            rover.Move(Instruction.M);
+
+            Assert.Equal(newX, rover.X);
+            Assert.Equal(newY, rover.Y);
+        }
+
+        [Theory]
+        [InlineData(Heading.N)]
+        [InlineData(Heading.E)]
+        [InlineData(Heading.S)]
+        [InlineData(Heading.W)]
+        public void Move_Forward_ShouldNotUpdateHeading(Heading heading)
+        {
+            var rover = new Rover(1, 2, heading);
+
+            rover.Move(Instruction.M);
+
+            Assert.Equal(heading, rover.Z);
+        }
+
+        [Theory]
         [InlineData("3 5 E")]
         [InlineData("0 0 N")]
         [InlineData("1 10 W")]
